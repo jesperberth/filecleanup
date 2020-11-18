4b7df722626240145e047588ed67ddd9765ec199
Jesper Berth <jesper.berth@arrow.com>
2020-11-18 11:05:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# File Cleanup
+
+Python script to clean up files not used in xx days
---
+++ filecleanup.py
@@ -0,0 +1,5 @@
+# File cleanup 0.0.1 - 18/11/2020
+# Author  Jesper.berth@arrow.com
+# 
+
+print("File Cleanup")
