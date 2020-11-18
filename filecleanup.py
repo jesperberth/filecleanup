9a4597aea8dd16f30cd07d997d3d5769baba7c74
Jesper Berth <jesper.berth@arrow.com>
2020-11-18 11:16:33 +0100

    test

--- filecleanup.py
+++ filecleanup.py
@@ -2,5 +2,16 @@
 # File cleanup 0.0.1 - 18/11/2020
 # Author  Jesper.berth@arrow.com
 # 
+import os
+import stat
+
+storagepath = "/mnt/transport/IT/._anyconnect-macos-4.9.04043-predeploy-k9.dmg"
 
 print("File Cleanup")
+
+def getLastAccess():
+    fileStatsObj = os.stat ( storagepath )
+    accessTime = time.ctime ( fileStatsObj [ stat.ST_ATIME ] )
+    print(accessTime)
+
+getLastAccess()
