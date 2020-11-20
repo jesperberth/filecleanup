b3d15aefbf0da7619728f72e4a23c20af00b56f8
Jesper Berth <jesper.berth@arrow.com>
2020-11-20 14:11:42 +0100

    test html body

--- filecleanup.py
+++ filecleanup.py
@@ -139,12 +139,12 @@ def statusMessage():
     """.format(filecount, dircount, storagepath, startTime, endTime, freedDisk, availAfterClean)
 
     # Turn these into plain/html MIMEText objects
-    part1 = MIMEText(text, "plain")
+    #part1 = MIMEText(text, "plain")
     part2 = MIMEText(html, "html")
 
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
-    message.attach(part1)
+    #message.attach(part1)
     message.attach(part2)
     attachFile = logfilename
 
